fd2f8c937578b7abf02049d150ae8823dd10de75
Josh Thornton <josh@speedscale.com>
2021-08-17 11:53:25 -0400

    backup

--- bkp.sh
+++ bkp.sh
@@ -11,6 +11,8 @@ cp ~/.config/karabiner/karabiner.json ./karabiner.json
 echo 'copying zshrc...'
 grep '### personal ###' ~/.zshrc -A 1000 > zshrc
 
+echo 'done'
+echo
 git add --all
 git commit -m "backup"
 git push
